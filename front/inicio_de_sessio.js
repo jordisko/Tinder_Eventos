99e1b7a92f10b780d7058d2a24b37cfe8808a431
marc <marc.hannouti360@gmail.com>
2022-09-08 13:57:36 +0200

    commit

--- front/inicio_de_sessio.js
+++ front/inicio_de_sessio.js
@@ -1,7 +1,8 @@
 const buttonsend= document.getElementById('buttonsend')
-const navigate = useNavigate()
 
 
 buttonsend.onclick = function(){
-    document.location.href = "perfil.html"
+    const navigate =useNavigate()
+    navigate('/perfil');
+
 }
