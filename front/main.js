a9801d2badcfd92571f6c1aa4c707ca5ad9ec1cf
Pedro Monje <pedro_es_1829@hotmail.com>
2022-09-09 13:59:31 +0200

    tercer commit

---
+++ front/main.js
@@ -0,0 +1,13 @@
+let nombres = ["Rocio Mejia", "Basilio Roca", "Mercedes Vela", "Isaias Wu", "Alexander Cobo"];
+
+var titulo = document.getElementById("titulo");
+
+console.log(titulo);
+
+
+
+    
+    
+
+
+
