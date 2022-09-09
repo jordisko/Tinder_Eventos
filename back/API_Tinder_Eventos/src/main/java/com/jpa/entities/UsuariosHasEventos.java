027d9babe41fc74aa060582f5c0c460bb069f690
joank <jjperdiguer@gmail.com>
2022-09-09 13:56:40 +0200

    Agregada clases

---
+++ back/API_Tinder_Eventos/src/main/java/com/jpa/entities/UsuariosHasEventos.java
@@ -0,0 +1,31 @@
+package com.jpa.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "usuarios")
+public class UsuariosHasEventos {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	@Column(name = "Usuarios_idUsuario")
+	Long usuarios_idUsuario;
+	
+	@Column(name = "Eventos_idEvento")
+	Long eventos_idEvento;
+	
+	@Column(name = "estadoMatch")
+	Long estadoMatch;
+	
+	@Column(name = "estadoEvento")
+	Long estadoEvento;
+
+}
