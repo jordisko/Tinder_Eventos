11d06f1ae4027bcf4f46d0ee3b01fe14af64be43
jordi campos <jordicampos.97@gmail.com>
2022-09-12 13:13:39 +0200

    Repositorio y Servicio mejorado

--- back/API_Tinder_Eventos/src/main/java/com/jpa/repository/UsuarioHasEventoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jpa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.jpa.entities.UsuarioHasEvento;
-
-@Repository
-public interface UsuarioHasEventoRepository extends JpaRepository<UsuarioHasEvento, Long> {}
