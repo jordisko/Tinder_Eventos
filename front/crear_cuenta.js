7e526f0b9a18a49271039b3828d406d22e876ac3
marc <marc.hannouti360@gmail.com>
2022-09-13 12:37:08 +0200

    commit

--- front/crear_cuenta.js
+++ front/crear_cuenta.js
@@ -1,16 +1,13 @@
 
 
+let todocorrecto = true;
+let formulario =document.formulariocontato
 
+for (let index = 0; index == formulario.length; index++) {
+if(formulario[i].value == null || formulario[i].value.length > 0){
+}
+}
 function cuentacrear(){
-  $("#formulario").submit(event => {
-    event.preventDefault();
-    alert("formulario comprobado");
-    document.location.href = "inicio.html"
-
-  
-  
-  })
-   
+  document.location.href = "inicio.html"
 
   }
-  
