f96fce7edc9cd5966c7fd1ebb77b5a97ab52abde
marc <marc.hannouti360@gmail.com>
2022-09-14 12:45:30 +0200

    commit

--- front/crear_cuenta.js
+++ front/crear_cuenta.js
@@ -1,13 +1,29 @@
 
 
-let todocorrecto = true;
-let formulario =document.formulariocontato
+// var todocorrecto = true;
+// var formulario =document.formulariocontato
 
-for (let index = 0; index == formulario.length; index++) {
-if(formulario[i].value == null || formulario[i].value.length > 0){
-}
-}
+// for (let index = 0; index == formulario.length; index++) {
+// if(formulario[i].value == null || formulario[i].value.length == 0){
+//   todocorrecto=false;
+// }
+
+// }
+// if(todocorrecto==true){
+//   console.log("todo correcto")
+// }
 function cuentacrear(){
   document.location.href = "inicio.html"
 
   }
+
+//   var nombre =document.getElementById('usuario')
+// eror.style.color ='red'
+// // function cuentacrear(){
+// //   var mensaje = []
+// //   if (nombre.value === null || nombre.value === '') {
+// //     mensaje.push('error')
+// //   } else {
+// //     document.location.href = "inicio.html"
+// //   }
+// //   }
