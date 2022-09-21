895fc8695d62788d1c1510578cb22c97f8e1ec7b
marc <marc.hannouti360@gmail.com>
2022-09-21 09:51:32 +0200

    commit

--- front/crearevento.js
+++ front/crearevento.js
@@ -5,3 +5,30 @@ function cancelar(){
 function crear(){
   document.location.href = "evento.html"
 }
+
+function mostrar(){
+  var archivo = document.getElementById("file").files[0];
+  var reader = new FileReader();
+  if (file) {
+    reader.readAsDataURL(archivo );
+    reader.onloadend = function () {
+      document.getElementById("img").src = reader.result;
+    }
+  }
+}
+function init() {
+  var inputFile = document.getElementById('inputFile1');
+  inputFile.addEventListener('change', mostrarImagen, false);
+}
+
+function mostrarImagen(event) {
+  var file = event.target.files[0];
+  var reader = new FileReader();
+  reader.onload = function(event) {
+    var img = document.getElementById('img1');
+    img.src= event.target.result;
+  }
+  reader.readAsDataURL(file);
+}
+
+window.addEventListener('load', init, false);
