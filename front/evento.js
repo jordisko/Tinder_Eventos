5b3e57e9465aee5f257074a18625d426e6ef89ba
marc <marc.hannouti360@gmail.com>
2022-09-29 11:24:08 +0200

    commi

--- front/evento.js
+++ front/evento.js
@@ -1,11 +1,11 @@
-function comprobarLogin() {
-    let usuarioLogged = localStorage.getItem("usuario");
-    if(usuarioLogged == '' || usuarioLogged == null)
-    {
-        alert('¡Tienes que iniciar sesión!');
-        window.location.href="inicio_de_session.html";
-    }
-}
+// function comprobarLogin() {
+//     let usuarioLogged = localStorage.getItem("usuario");
+//     if(usuarioLogged == '' || usuarioLogged == null)
+//     {
+//         alert('¡Tienes que iniciar sesión!');
+//         window.location.href="inicio_de_session.html";
+//     }
+// }
 
 function crear(){
     document.location.href = "crearevento.html"
