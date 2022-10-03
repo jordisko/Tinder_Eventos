0591f3bb286de2fa9f9630fb5d4640f69f827da9
marcelhannouti <37220618+marcelhannouti@users.noreply.github.com>
2022-10-03 15:26:28 +0200

    commit

--- front/pagina_principal.js
+++ front/pagina_principal.js
@@ -11,3 +11,6 @@ function cerrar(){
     document.location.href = "inicio_de_session.html"
     localStorage.removeItem("usuario");
 }
+function perfilevento(){
+    document.location.href = "perfilevento.html"
+}
